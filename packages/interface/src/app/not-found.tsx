1f96c8d60f4c67fd7068fe2a5f2abbea6148514d
yliu <yangboses@gmail.com>
2024-10-23 11:22:37 +0800

    style: 对404页面偏移修复

--- packages/interface/src/app/not-found.tsx
+++ packages/interface/src/app/not-found.tsx
@@ -4,7 +4,7 @@ import img404 from '@/assets/img/404.svg';
 
 export default function NotFound() {
   return (
-    <div className="flex items-center justify-center flex-col w-100vw h-100vh pos-fixed select-none">
+    <div className="flex items-center justify-center flex-col w-1400px h-100vh pos-fixed select-none">
       <Image src={img404} width={450} height={240} alt="404" draggable={false}></Image>
       <p className="font-400 text-4.5 lh-5.27 color-#999 ">
         找不到相关页面，请检查输入URL是否正确，点击
